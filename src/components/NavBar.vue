e17d846238ab38fb7f2ce3a2b055a871e047a6b9
Víctor Cámara <73994759+VICamaraPrg@users.noreply.github.com>
2024-06-13 22:26:18 +0200

    Setup main page along FavBooks page

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -19,50 +19,6 @@
           </div>
         </div>
 
-        <div class="hidden md:block">
-          <div class="ml-4 flex items-center md:ml-6">
-            <button
-              class="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-gray-700"
-              aria-label="Notifications"
-            >
-              <svg
-                class="h-6 w-6"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                ></path>
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M15 8a5 5 0 00-5-5m0 10a5 5 0 015-5m-5 5h5m2 0h1m-1 0a5 5 0 00-5-5h-2a5 5 0 00-5 5"
-                ></path>
-              </svg>
-            </button>
-
-            <div class="ml-3 relative">
-              <div>
-                <button
-                  class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                  id="user-menu"
-                  aria-haspopup="true"
-                >
-                  <span class="sr-only">Open user menu</span>
-                  <img
-                    class="h-8 w-8 rounded-full"
-                    alt=""
-                  />
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
         <div class="-mr-2 flex md:hidden">
           <button
             class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
