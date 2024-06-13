8e23ddf06fba57de8776f15bff1c90e4fc2231bf
Víctor Cámara <73994759+VICamaraPrg@users.noreply.github.com>
2024-06-13 22:25:37 +0200

    feat: Setup main components

---
+++ src/components/BookCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bg-white shadow-md rounded-lg p-4 flex-1 h-full">
+    <h3 class="text-xl font-semibold">{{ book.title }}</h3>
+    <p class="text-gray-500">{{ book.author }}</p>
+    <p class="text-gray-500">{{ book.genre }}</p>
+    <p class="text-gray-500">{{ book.year }}</p>
+    <div class="flex justify-end mt-4 gap-2">
+      <button @click="bookStore.deleteBook(book.id)">
+        <TrashIcon />
+      </button>
+      <button @click="bookStore.toggleBookToFavorites(book.id)">
+        <HeartIcon :isFav="book.isFav" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import HeartIcon from '@/assets/icons/HeartIcon.vue';
+import TrashIcon from '@/assets/icons/TrashIcon.vue';
+import type { Book } from '@/core/interfaces/Book';
+import { useBookStore } from '@/stores/book-store';
+import { defineProps } from 'vue';
+
+const bookStore = useBookStore();
+
+defineProps<{
+  book: Book;
+}>();
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,137 @@
+<template>
+  <nav class="bg-gray-800">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex items-center justify-between h-16">
+        <div class="flex items-center">
+          <div class="hidden md:block">
+            <div class="ml-10 flex items-baseline space-x-4">
+              <RouterLink
+                to="/"
+                class="text-gray-300 hover:cursor-pointer hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >Home</RouterLink
+              >
+              <RouterLink
+                to="/my-favorite-books"
+                class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >My favorite books</RouterLink
+              >
+            </div>
+          </div>
+        </div>
+
+        <div class="hidden md:block">
+          <div class="ml-4 flex items-center md:ml-6">
+            <button
+              class="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-gray-700"
+              aria-label="Notifications"
+            >
+              <svg
+                class="h-6 w-6"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                ></path>
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 8a5 5 0 00-5-5m0 10a5 5 0 015-5m-5 5h5m2 0h1m-1 0a5 5 0 00-5-5h-2a5 5 0 00-5 5"
+                ></path>
+              </svg>
+            </button>
+
+            <div class="ml-3 relative">
+              <div>
+                <button
+                  class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                  id="user-menu"
+                  aria-haspopup="true"
+                >
+                  <span class="sr-only">Open user menu</span>
+                  <img
+                    class="h-8 w-8 rounded-full"
+                    alt=""
+                  />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="-mr-2 flex md:hidden">
+          <button
+            class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+            aria-expanded="false"
+            @click="showMenu = !showMenu"
+          >
+            <svg
+              class="block h-6 w-6"
+              xmlns="https://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+              v-if="!showMenu"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+
+            <svg
+              class="h-6 w-6"
+              xmlns="https://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+              v-if="showMenu"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div
+      class="md:hidden"
+      id="mobile-menu"
+      v-show="showMenu"
+    >
+      <div
+        class="flex flex-col justify-center items-center px-2 pt-2 pb-3 space-y-1 sm:px-3"
+      >
+        <RouterLink
+          to="/"
+          class="text-gray-300 hover:cursor-pointer hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+          >Home</RouterLink
+        >
+        <RouterLink
+          to="/my-favorite-books"
+          class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+          >My favorite books</RouterLink
+        >
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const showMenu = ref(false);
+</script>
